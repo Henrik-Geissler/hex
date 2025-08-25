1993c5245aa69f2ee5d77e9945063e748d0237bd
Henrik Geißler <62219051+Henrik-Geissler@users.noreply.github.com>
2025-08-25 11:41:43 +0200

    Update ColorUpgrader.ts

--- src/relicts/ColorUpgrader.ts
+++ src/relicts/ColorUpgrader.ts
@@ -1,7 +1,7 @@
 import { Relict } from '../types/Relict';
 import { Tile } from '../types/Tile';
 import { handleUpgrade } from '../utils/mutations/handleUpgrade';
-
+import { Rarety } from '../types/Rarety';
 export class ColorUpgrader implements Relict {
   name: string = 'Color Upgrader';
   description: string = 'When a Relict changes its color, upgrade it';
